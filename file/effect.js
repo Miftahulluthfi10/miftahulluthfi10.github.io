3d1915829aef760bf02fd1ba6b819853390fe6a7
Miftaghul Luthfi <64729342+Miftahulluthfi10@users.noreply.github.com>
2022-09-20 17:36:21 +0700

    Update effect.js

--- file/effect.js
+++ file/effect.js
@@ -151,11 +151,11 @@ $('document').ready(function(){
 		$('#b6').attr('id','b66')
 		$('#b7').attr('id','b77')
 		$('#b11').animate({top:240, left: vw-250},500);
-		$('#b22').animate({top:240, left: vw-200},500);
-		$('#b33').animate({top:240, left: vw-100},500);
-		$('#b44').animate({top:240, left: vw-40},500);
-		$('#b55').animate({top:240, left: vw+40},500);
-		$('#b66').animate({top:240, left: vw+100},500);
+		$('#b22').animate({top:240, left: vw-70},500);
+		$('#b33').animate({top:240, left: vw-30},500);
+		$('#b44').animate({top:240, left: vw-0},500);
+		$('#b55').animate({top:240, left: vw+50},500);
+		$('#b66').animate({top:240, left: vw+110},500);
 		$('#b77').animate({top:240, left: vw+150},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
